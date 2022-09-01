68232dad20bd0aa752c5a95a48d1ca494b1cc3af
keogami <41939011+keogami@users.noreply.github.com>
2022-09-01 18:40:13 +0530

    created analysis code for processing data

--- js/modules/chart.js
+++ js/modules/chart.js
@@ -1,5 +1,24 @@
+const NAME = 0
+const WEIGHT = 1
+
+const sort = data => data.sort((a, b) => a[WEIGHT] - b[WEIGHT])
+
+const median = sortedWeights => {
+  const mid = sortedWeights.length / 2
+  if (sortedWeights.length % 2 === 1) { // odd
+    return sortedWeights[mid - 0.5]
+  }
+  // even
+  return (sortedWeights[mid - 1] + sortedWeights[mid]) / 2
+}
+
+const getWeights = data => data.map(item[WEIGHT])
+
+const analyze = sortedWeights => [sortedWeights[0], median(sortedWeights), sortedWeights[sortedWeights.length - 1]]
+
 const generateChart = (container, data) => {
-  console.log(container, data)
+  const sortedData = sort(data)
+  const [min, median, max] = analyze(getWeights(sortedData))
 }
 
 export default generateChart

9e3a1f71dec6bfb6b2f256cfdb59b6227652996c
Abhinav Verma <abhinaverma9630@gmail.com>
2022-09-02 17:25:48 +0530

    implemented results

--- js/view.js
+++ js/view.js
@@ -1,10 +1,7 @@
 const average = arr => arr.reduce((a, b) => a + b) / arr.length
 
 const extractTopics = obj => {
-    // TODO extract all topic names and all weights and return them as an array
-    // return [
-    //    ["topic name", [15, 3, 13]]
-    // ]
+   
     const itemList = []
     for (let i in obj.topic) {
         const topic = obj.topic[i]
@@ -21,7 +18,7 @@ const extractTopics = obj => {
     }
 
     return itemList    
-}
+ }
 const testExtractTopics = topicList => {
     topicList.map(([name, weights]) => {
         console.assert(typeof name === "string", "the first element of topicList should be a string")
@@ -33,16 +30,14 @@ const testExtractTopics = topicList => {
 }
 
 const createWeightedList = topicList => {
-    // TODO take a topiclist and average out the weights
-    // extractTopics' output
-    // topicList = [
-    //    ["topic name", [15, 3, 12]],
-    //    ["topic name 2", [7, 3]],
-    // ]
-    // result = [
-    //    ["topic name", 10],
-    //    ["topic name 2", 5],
-    // ]
+    
+    for  (let i in average){
+        const avg = average[i]
+        const result =[ topic.name , average]
+        topicList.push(result);
+    }
+    return topicList
+
 }
 
 const handleData = obj => {
@@ -54,7 +49,7 @@ const handleData = obj => {
 }
 
 // fetch data and generate the task list and the charts
-fetch('https://raw.githubusercontent.com/quasilevel/ccsu-opendata/main/BCA/103.json')
+fetch('https://raw.githubusercontent.com/quasilevel/ccsu-opendata/main/BCA/302.json')
 .then(response => {
     return response.json();
 }).then(handleData).catch(error => {

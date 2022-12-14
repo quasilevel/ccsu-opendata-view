092f2fae0f7aedc3ff29191ac976dedd4c78baf9
keogami <41939011+keogami@users.noreply.github.com>
2022-12-14 23:24:11 +0530

    docs(index.js): explain the animation sequence

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import anime from '/js/modules/anime.es.js'
 
 const grey = getComputedStyle(document.body).getPropertyValue("--grey")
 
+// states describes the color and size animation states described below
 const states = [
   [true, true],
   [true, false],
@@ -9,17 +10,27 @@ const states = [
   [false, false],
 ]
 
-const play = (forward=0, rand=0) => {
-  const [color, scale] = states[forward]
-  const next = () => play((forward + 1) % 4, (forward % 2 === 1) ? anime.random(0, 8 * 6) : rand)
+/**
+ * play runs the staggered grid animation on the home page.
+ * 
+ * The animation changes color of the dots and the size of the dots in an alternating pattern.
+ * The animation loops through the states encoded in the states variable.
+ * Every other iteration, a new random dot's index is choosen as the center of the staggered effect.
+ */
+const play = (state=0, dotIdx=0) => {
+  const [color, scale] = states[state] // retrieve the color direction, and the scale direction
+
+  // next runs the next iteration of the animation, moving the state forward by a step (looping back to the first after last state).
+  // it chooses a random dot every other iteration, or simply uses the previous dot
+  const next = () => play((state + 1) % states.length, (state % 2 === 1) ? anime.random(0, 8 * 6) : dotIdx)
   anime({
     targets: '#dots div',
-    backgroundColor: color ? [grey, '#000'] : ['#000', grey],
-    scale: scale ? [1, 0.5] : [0.5, 1],
+    backgroundColor: color ? [grey, '#000'] : ['#000', grey], // alternates between going from grey to black and from black to grey based on state
+    scale: scale ? [1, 0.5] : [0.5, 1], // similarly for the size
     easing: 'linear',
     duration: 800,
-    delay: anime.stagger(250, {grid: [8, 6], from: rand}),
-    complete: async () => (forward % 2 === 0) ? next() : setTimeout(next, 4000)
+    delay: anime.stagger(250, {grid: [8, 6], from: dotIdx}), // set the selected dot as the center for the stagger
+    complete: async () => (state % 2 === 0) ? next() : setTimeout(next, 4000) // wait 4seconds before moving to the next iteration on every other iteration
   })
 }
 
